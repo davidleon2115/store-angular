8cab7daf97568c4b639fb16fc638a35c58ec365a
davidleon2115 <leon95024@gmail.com>
2023-06-06 22:33:04 -0500

    Event Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'store-angular';
+    name = 'Nicolas';
+    age = 18;
+    img = 'https://www.w3schools.com/howto/img_avatar.png';
+    btnDisabled = true;
+    person = {
+      name: 'Nicolas',
+      age: 18,
+      avatar: 'https://www.w3schools.com/howto/img_avatar.png'
+    }
+
+    toggleButton() {
+      this.btnDisabled = !this.btnDisabled;
+    }
+
+    increaseAge() {
+      this.person.age += 1;
+    }
 }
